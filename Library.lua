6170e2a5ea18b856e5b47c997ca6910c8b8091db
PollutedHub <whysosad213404@outlook.com>
2026-06-12 23:46:36 +0100

    Library.lua

--- Library.lua
+++ Library.lua
@@ -6898,7 +6898,7 @@ function Library:CreateWindow(WindowInfo)
             Parent = BottomBar,
         })
 
-local LocalVersion = "1.0.3"
+local LocalVersion = "1.0.4"
 
 
         -- Status Circle
