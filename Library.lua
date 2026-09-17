cfd5f6cd99d4a18441a6a0ac60b11897875be297
PollutedHub <whysosad213404@outlook.com>
2026-09-17 18:16:27 +0100

    Library.lua

--- Library.lua
+++ Library.lua
@@ -1,6 +1,6 @@
 local cloneref = (cloneref or clonereference or function(instance: any)
     return instance
-end)
+end) 
 local CoreGui: CoreGui = cloneref(game:GetService("CoreGui"))
 local Players: Players = cloneref(game:GetService("Players"))
 local RunService: RunService = cloneref(game:GetService("RunService"))
